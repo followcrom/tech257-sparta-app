6953664b9b5ae19a4ef4ea11716dab98909e6c27
followCrom <followcrom@gmail.com>
2024-03-08 16:19:26 +0000

    Update sparta-app-setup.sh: Clone app repository and start with pm2

--- sparta-app-setup.sh
+++ sparta-app-setup.sh
@@ -16,11 +16,16 @@ curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
 # Clone your app repository (replace YOUR_GIT_REPO_URL with your actual Git repository URL)
-git clone YOUR_GIT_REPO_URL
+git clone https://github.com/followcrom/tech257-sparta-app.git
 
 # Assuming your repository's name is 'app', if not, change the directory name accordingly
 cd app
 
 # Install dependencies and run your app
 npm install
-node app.js
+
+# Install pm2 globally
+sudo npm install pm2@latest
+
+# Use pm2 to start your app and ensure it runs in the background
+pm2 start app.js --name "sparta-test-app"
